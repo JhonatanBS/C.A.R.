42c2be7889baf03bb91ab6f980caf0c71a796d76
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2026-04-21 13:02:40 -0300

    Feat: Add Driver entity and REST API with Spring Boot and H2 database

---
+++ src/main/kotlin/com/car/appCar/domain/Repositories.kt
@@ -0,0 +1,5 @@
+package com.car.appCar.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface DriverRepository : JpaRepository<Driver,Long>
---
+++ src/main/kotlin/com/car/appCar/domain/entities.kt
@@ -0,0 +1,16 @@
+package com.car.appCar.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.time.LocalDate
+
+@Entity
+data class Driver(
+
+    @Id
+    var id: Long? = null,
+
+    val name: String,
+
+    val birthDate: LocalDate
+)
